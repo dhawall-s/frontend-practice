1da2b1ea84237b37db2b2ab90f565ea476676c26
dhawall-s <sainidhawal755@gmail.com>
2024-08-09 23:37:03 +0530

    practice

--- javascript_practice/maths.js
+++ javascript_practice/maths.js
@@ -1,31 +1,55 @@
-console.log(2*2);
-console.log(2+2);
-console.log(2-2);
-console.log(2%2);
-console.log(2/2);
 
-let counter=0
-counter++;
-console.log(counter);
+// console.log(2*2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2/2);
 
-counter--
-console.log(counter);
+// let counter=0
+// counter++;
+// console.log(counter);
 
-let favnum=45;
-let secondFavnum=18;
-let add = favnum+secondFavnum;
-let sub= favnum-secondFavnum;
-let multiply= favnum*secondFavnum;
-let divide= favnum/secondFavnum;
+// counter--
+// console.log(counter);
 
-let mod=secondFavnum%favnum;
-let power=favnum ** secondFavnum;
+// let favnum=45;
+// let secondFavnum=18;
+// let add = favnum+secondFavnum;
+// let sub= favnum-secondFavnum;
+// let multiply= favnum*secondFavnum;
+// let divide= favnum/secondFavnum;
 
-console.log(add)
-console.log(sub)
-console.log(multiply)
-console.log(divide)
-console.log(mod)
-console.log(power)
+// let mod=secondFavnum%favnum;
+// let power=favnum ** secondFavnum;
 
-console.log('hello world')
+// console.log(add)
+// console.log(sub)
+// console.log(multiply)
+// console.log(divide)
+// console.log(mod)
+// console.log(power)
+// console.log('hello world')
+
+
+let x=3;
+let y=4;
+let z=5;
+
+let c;
+
+// Math.round(x)
+// Math.floor(y)
+// Math.ceil(z)
+// Math.sign(x)
+console.log(Math.round(x));
+console.log(Math.floor(y));
+console.log(Math.ceil(z));
+console.log(Math.sign(x));
+console.log(Math.trunc(x));
+console.log(Math.pow(x , y));
+console.log(Math.abs(-5));
+console.log(Math.sin(90));
+console.log(Math.cos(90));
+console.log(Math.tan(90));
+console.log (Math.floor(Math.random()*10));
+ 
